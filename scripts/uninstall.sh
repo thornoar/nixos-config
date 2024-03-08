ad9713550a9154f8657a4e1c2b82dac9072f67d6
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-08 16:34:36 +0100

    --

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,10 +1,10 @@
-echo -e "\[35m| Unistalling Package \"$1\"|\[0m"
+echo -e "\e[35m| Unistalling Package \"$1\"|\e[0m"
 if grep -q $1 "$NIXOS_CONFIG/home-packages"; then
-    echo -e "\[34m> Removing \"$1\" from package list...\[0m"
+    echo -e "\e[34m> Removing \"$1\" from package list...\e[0m"
     awk "!/$1/" "$NIXOS_CONFIG/home-packages" > temp && mv temp "$NIXOS_CONFIG/home-packages"
-    echo -e "\[34m> Rebuilding system configuration...\[0m"
+    echo -e "\e[34m> Rebuilding system configuration...\e[0m"
     srb
 else
-	echo -e "\[34m> Package already removed from configuration. Run \"sudo nixos-rebuild switch to uninstall. Quitting...\"\[0m"
+	echo -e "\e[34m> Package already removed from configuration. Run \"sudo nixos-rebuild switch to uninstall. Quitting...\"\e[0m"
 	exit 0
 fi
