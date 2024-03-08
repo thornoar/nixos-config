8f70300f5a38a1e29c0ac8dde0d4b3640d68ae31
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-08 14:43:17 +0100

    --

--- home-scripts.nix
+++ home-scripts.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 {
-    home.packages = lib.lists.forEach (lib.filesystem.listFilesRecursive ./dotfiles/scripts) (filename:
+    home.packages = lib.lists.forEach (lib.filesystem.listFilesRecursive ./scripts) (filename:
         pkgs.writeScriptBin
         (lib.strings.removeSuffix ".sh"  (
             lib.lists.last (lib.strings.splitString "/" (builtins.toString filename))
@@ -10,4 +10,4 @@
     );
 }
 
-# [./dotfiles/scripts]
+# [./scripts]
