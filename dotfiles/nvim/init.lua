eb9e9ea1a8d2367905d72ea07918a08e56ac3a14
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-09 00:46:49 +0100

    --

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -383,6 +383,25 @@ require('nvim-treesitter.configs').setup {
 			node_decremental = '<M-space>',
 		},
 	},
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            },
+            selection_modes = {
+                ['@parameter.outer'] = 'v', -- charwise
+                ['@function.outer'] = 'V', -- linewise
+                ['@class.outer'] = '<c-v>', -- blockwise
+            },
+            include_surrounding_whitespace = true,
+        },
+    },
 }
 -- $lualine setup
 local function keymap()
