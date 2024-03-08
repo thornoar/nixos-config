56c430934abec683f299c1747e0eb6459fabece9
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-08 19:42:07 +0100

    --

--- scripts/gcollect.sh
+++ scripts/gcollect.sh
@@ -1,1 +1,5 @@
-nix-collect-garbage --delete-old && sudo nix-collect-garbage --delete-old
+echo -e "\e[35m| Collect Garbage |\e[0m"
+echo -e "\e[34m> Collecting on the user level...\e[0m"
+nix-collect-garbage --delete-old
+echo -e "\e[34m> Collecting on the root level...\e[0m"
+sudo nix-collect-garbage --delete-old
