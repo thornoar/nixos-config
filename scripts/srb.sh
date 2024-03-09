55f09b6dc0a695f40fa876160e0834be7716115a
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-09 13:20:19 +0100

    --

--- scripts/srb.sh
+++ scripts/srb.sh
@@ -3,7 +3,7 @@ export COMMAND="switch"
 export EXTRA_OPTIONS=""
 export OUTPUT="master"
 
-export GREP_COLORS="ms=1;94"
+grep_colors=$GREP_COLORS
 
 while getopts "hpic:e:o:" flag; do
     case $flag in
@@ -49,14 +49,18 @@ if $CHECK_GIT; then
         echo -e "\e[34m> Checking git repository on branch \e[33m$(git rev-parse --abbrev-ref HEAD)\e[34m...\e[0m"
         git remote update
         if git status | grep -q "branch is up to date"; then
+            export GREP_COLORS="ms=1;94"
             git status | grep --color "branch is up to date"
+            export GREP_COLORS=$grep_colors
             echo "local changes:"
             git status -s
             echo -e "\e[34m> Pushing changes...\e[0m"
             git add . && git commit -m '--' && git push
         elif git status | grep -q "Changes not staged for commit"; then
+            export GREP_COLORS="ms=1;91"
             git status | grep "behind"
             git status | grep "not staged"
+            export GREP_COLORS=$grep_colors
             echo -e "\e[34m> Clash between local and remote branches, skipping...\e[0m"
         else
             echo -e "\e[34m> Pulling remote changes...\e[0m"
@@ -73,5 +77,3 @@ sudo nixos-rebuild "$COMMAND" "$EXTRA_OPTIONS" --flake $NIXOS_CONFIG/#master || 
 echo -e "\e[34m> Recompiling XMonad...\e[0m"
 killall xmobar
 recompile_xmonad && xmonad --restart
-
-export GREP_COLORS=""
