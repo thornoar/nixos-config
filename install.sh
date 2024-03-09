dbfd0da5f988bbcc695587288fbdc9a43f4fb3a0
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-09 15:01:28 +0100

    --

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-sudo chown -R $USER:root /etc/nixos/
+printf "\e[35| NixOS Configuration Install Script |\e[0m\n"
+sudo chown -R "$USER":root /etc/nixos/
 sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
 sudo cp ./dotfiles/template.nix /etc/nixos/system-options.nix
 sudo cp ./dotfiles/template.nix /etc/nixos/home-options.nix
-echo "NixOS setup is ready for use!"
-echo "Call \"sudo nixos-rebuild switch --impure --flake $PWD#master\" to update system."
-echo "Call \"home-manager switch --flake $PWD\" to update the user space."
+printf "\e[34mNixOS setup is ready for use!\e[0m\n"
+printf "\e[34mCall \"sudo nixos-rebuild switch --impure --flake %s#master\" to update system.\e[0m\n" "$PWD"
+printf "\e[34mCall \"home-manager switch --flake %s\" to update the user space.\e[0m\n" "$PWD"
