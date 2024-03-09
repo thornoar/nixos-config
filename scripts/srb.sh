92c39a937c795558b991b653c7d16e8016c22433
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-09 14:56:24 +0100

    --

--- scripts/srb.sh
+++ scripts/srb.sh
@@ -63,26 +63,26 @@ if $CHECK_GIT; then
                 printf "\e[34m> Pushing changes...\e[0m\n"
                 git add . && git commit -m '--' && git push
             fi
-            export GREP_COLORS=$grep_colors
+            export GREP_COLORS="$grep_colors"
         elif git status | grep -q "Changes not staged for commit"; then
             export GREP_COLORS="ms=1;91"
             git status | grep --color "behind"
             git status | grep --color "not staged"
-            export GREP_COLORS=$grep_colors
+            export GREP_COLORS="$grep_colors"
             printf "\e[34m> Clash between local and remote branches, skipping...\e[0m\n"
         else
             export GREP_COLORS="ms=1;91"
             git status | grep --color "behind"
-            export GREP_COLORS=$grep_colors
+            export GREP_COLORS="$grep_colors"
             printf "\e[34m> Pulling remote changes...\e[0m\n"
             git fetch && git pull
         fi
     fi
-    cd $cwd
+    cd "$cwd" || exit
 fi
 
 printf "\e[34m> Building configuration...\e[0m\n"
-sudo nixos-rebuild $COMMAND $EXTRA_OPTIONS --flake $NIXOS_CONFIG/#master || exit 1
+sudo nixos-rebuild "$COMMAND" "$EXTRA_OPTIONS" --flake "$NIXOS_CONFIG"/#master || exit 1
 printf "\e[34m> Recompiling XMonad...\e[0m\n"
 
 killall xmobar
