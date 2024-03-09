dd38d9495041c83a57030623e45b0a9bc2950a7f
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-09 16:53:57 +0100

    --

--- scripts/gitpush.sh
+++ scripts/gitpush.sh
@@ -3,6 +3,9 @@ grep_colors="$GREP_COLORS"
 printf "\e[34m> Pushing to remote repository (branch \e[33m%s\e[34m)...\e[0m\n" "$(git rev-parse --abbrev-ref HEAD)"
 git remote update
 if git status | grep -q "branch is up to date"; then
+    export GREP_COLORS="ms=1;94"
+    git status | grep --color "branch is up to date"
+    export GREP_COLORS="$grep_colors"
     git add . && git commit -m '--' && git push
 else
     export GREP_COLORS="ms=1;91"
