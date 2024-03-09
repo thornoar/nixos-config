5a1b864da91478bd0359c9c59a8b5a5b31879794
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-09 16:53:10 +0100

    --

--- scripts/gitpush.sh
+++ scripts/gitpush.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-printf "\e[34m> Pushing to remote repository...\e[0m\n"
-git add . && git commit -m '--' && git push
+grep_colors="$GREP_COLORS"
+printf "\e[34m> Pushing to remote repository (branch \e[33m%s\e[34m)...\e[0m\n" "$(git rev-parse --abbrev-ref HEAD)"
+git remote update
+if git status | grep -q "branch is up to date"; then
+    git add . && git commit -m '--' && git push
+else
+    export GREP_COLORS="ms=1;91"
+    git status | grep --color "behind"
+    export GREP_COLORS="$grep_colors"
+    printf "\e[34m> There are unpulled changes, please resolve all conflicts. Quitting...\e[0m\n"
+fi
