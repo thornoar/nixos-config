186b4a5065816b2f329541d667b733714a0abd99
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-10 14:31:34 +0100

    --

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,4 +1,4 @@
--- vim.loader.enable()
+vim.loader.enable()
 
 -- VARIABLES --
 vim.keymap.set('n', 'ec', ':e $NIXOS_CONFIG/dotfiles/nvim/init.lua<CR>')
@@ -35,7 +35,6 @@ require('lazy').setup({
 	'junegunn/fzf.vim',
     'nanozuki/tabby.nvim',
     'lewis6991/gitsigns.nvim',
-    'lewis6991/impatient.nvim',
     {
         'altermo/ultimate-autopair.nvim',
         event = { 'InsertEnter','CmdlineEnter' },
@@ -71,8 +70,6 @@ require('lazy').setup({
 	},
     -- 'thornoar/nvim-subfiles',
 }, {})
-
-require('impatient')
 
 -- COMMANDS --
 local compilefunc = {
