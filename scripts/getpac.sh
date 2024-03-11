ec57a947d76d3a502592af732f01f54ec2ae7148
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-11 17:59:09 +0100

    system update

--- scripts/getpac.sh
+++ scripts/getpac.sh
@@ -4,6 +4,6 @@ if grep -q "^$1$" "$NIXOS_CONFIG/home-packages"; then
 	exit 0
 fi
 nix search nixpkgs#$1 || exit 1
-printf "\e[34m> Prepending \"\e[0m\e[35m%s\e[0m\e[34m\" to package list...\e[0m\n" "$1"
+printf "\e[34m> Appending \"\e[0m\e[35m%s\e[0m\e[34m\" to package list...\e[0m\n" "$1"
 echo "$1" >> "$NIXOS_CONFIG/home-packages"
 printf "\e[34m> Rebuild your system for the package to be installed.\e[0m\n"
