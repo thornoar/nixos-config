9909da313564af2c74ec1d4a0da7e3c7d1142e8f
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-11 17:34:35 +0100

    system update

--- scripts/lstpac.sh
+++ scripts/lstpac.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
+
+highlight() {
+    escape=$(printf '\033')
+    sed "s,$2,${escape}[$1m&${escape}[0m,g"
+}
+
 printf "\e[35m| List Installed User Packages |\e[0m\n"
-export GREP_COLORS="ms=1;94"
 grep -v '^ *#' < "$NIXOS_CONFIG/home-packages" | while IFS= read -r package
 do
-    nix search nixpkgs#"$package" > /dev/null 2>&1
+    nix search nixpkgs#"$package" | highlight 33 "$package"
 done
-grep -v '^ *#' < "$NIXOS_CONFIG/home-packages" | while IFS= read -r package
-do
-    nix search nixpkgs#"$package" | grep --color "$package"
-done
-export GREP_COLORS=""
