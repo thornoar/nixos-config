45603d5c6c76f60e4a5746ee82a77b642ff7ed62
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-12 22:26:56 +0100

    system update

--- home-options.nix
+++ home-options.nix
@@ -59,7 +59,7 @@
 
         colorRed0 = mkStrOpt "#ff5555";
         colorRed1 = mkStrOpt "#e86671";
-        colorRed2 = mkStrOpt "#af5054";
+        colorRed2 = mkStrOpt "#ff9094";
 
         colorBlue0 = mkStrOpt "#2b6afa";
         colorBlue1 = mkStrOpt "#61afef";
