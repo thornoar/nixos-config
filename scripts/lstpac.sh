36d8b6599057765162406e57d1d63d17cba62cbe
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-12 23:00:03 +0100

    system update

--- scripts/lstpac.sh
+++ scripts/lstpac.sh
@@ -5,6 +5,11 @@ highlight() {
     sed "s,$2,${escape}[$1m&${escape}[0m,g"
 }
 
+grep -v '^ *#' < "$NIXOS_CONFIG/home-packages" | while IFS= read -r package
+do
+    nix search nixpkgs#"$package" > /dev/null 2>&1
+done
+
 printf "\e[34m> Listing installed user packages...\e[0m\n"
 grep -v '^ *#' < "$NIXOS_CONFIG/home-packages" | while IFS= read -r package
 do
