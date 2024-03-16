d20039fbd9a52834c156370209f1faa01919e31c
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-16 19:39:15 +0100

    --

--- scripts/gitupd.py
+++ scripts/gitupd.py
@@ -17,10 +17,14 @@ def run_command (command):
 # def is_git_directory (path = '.'):
 #     return subprocess.call(['git', '-C', path, 'status'], stderr=subprocess.STDOUT, stdout = open(os.devnull, 'w')) == 0
 
-if (subprocess.call(['git', '-C', '.', 'status'], stderr=subprocess.STDOUT, stdout = open(os.devnull, 'w')) == 0):
-    branch = run_command(["git", "rev-parse", "--abbrev-ref", "HEAD"])
-    os.system("git remote update")
-    status = run_command(["git", "status"])
+if (subprocess.call(
+        ['git', '-C', args.target, 'status'],
+        stderr = subprocess.STDOUT,
+        stdout = open(os.devnull, 'w')
+    ) == 0):
+    branch = run_command(["git", "-C", args.target, "rev-parse", "--abbrev-ref", "HEAD"])
+    os.system("git -C " + args.target + " remote update")
+    status = run_command(["git", "-C", args.target, "status"])
     print("\033[34m> Checking git repository on branch \033[33m" + branch + "\033[34m...\033[0m") #]]]]
     if "branch is up to date" in status:
         print("branch is \033[1;94mup to date\033[0m with remote repository") #]]
@@ -28,17 +32,17 @@ if (subprocess.call(['git', '-C', '.', 'status'], stderr=subprocess.STDOUT, stdo
             print("local working tree \033[1;94mclean\033[0m") #]]
         else:
             print("local changes:")
-            os.system("git status -s")
+            os.system("git -C " + args.target + " status -s")
             print("\033[34m> Pushing local changes...\033[0m") #]]
-            os.system("git add . && git commit -m \"" + args.message +"\" && git push")
+            os.system("git -C " + args.target + " add . && git -C " + args.target + " commit -m \"" + args.message +"\" && git -C " + args.target + " push")
     elif "not staged" in status:
         print("local branch falls \033[1;91mbehind\033[0m remote") #]]
         print("changes \033[1;91mnot staged\033[0m:") #]]
-        os.system("git status -s")
+        os.system("git -C " + args.target + " status -s")
         print("\033[error:\033[0m Clash between local and remote changes.") #]]
     else:
         print("local branch falls \033[1;94mbehind\033[0m remote") #]]
         print("\033[34m> Pulling remote changes...\033[0m") #]]
-        os.system("git fetch && git pull")
+        os.system("git -C " + args.target + " fetch && git -C " + args.target + " pull")
 else:
     print("\033[error:\033[0m Not a git repository.") #]]
