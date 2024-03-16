a6e1e08046b2d6ebe9a48a1f00d548244a962006
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-16 10:36:55 +0100

    --

--- scripts/parsecmd.py
+++ scripts/parsecmd.py
@@ -7,7 +7,10 @@ import os
 import sys
 
 if (len(sys.argv) >= 3):
-    os.system(parse(sys.argv[1], sys.argv[2]))
+    if "%" in sys.argv[1]:
+        os.system(parse(sys.argv[1], sys.argv[2]))
+    else:
+        os.system(sys.argv[1] + " " + sys.argv[2])
 elif (len(sys.argv) >= 2):
     os.system(sys.argv[1])
 else:
