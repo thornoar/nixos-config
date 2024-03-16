1d3d718a593c004655d0d29ded9724e74e49066d
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-16 19:45:06 +0100

    --

--- scripts/gitupd.py
+++ scripts/gitupd.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 import argparse
+import contextlib
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--target", type = str, default = ".", help = "specify the git directory")
@@ -17,32 +18,38 @@ def run_command (command):
 # def is_git_directory (path = '.'):
 #     return subprocess.call(['git', '-C', path, 'status'], stderr=subprocess.STDOUT, stdout = open(os.devnull, 'w')) == 0
 
-if (subprocess.call(
-        ['git', '-C', args.target, 'status'],
-        stderr = subprocess.STDOUT,
-        stdout = open(os.devnull, 'w')
-    ) == 0):
-    branch = run_command(["git", "-C", args.target, "rev-parse", "--abbrev-ref", "HEAD"])
-    os.system("git -C " + args.target + " remote update")
-    status = run_command(["git", "-C", args.target, "status"])
-    print("\033[34m> Checking git repository on branch \033[33m" + branch + "\033[34m...\033[0m") #]]]]
-    if "branch is up to date" in status:
-        print("branch is \033[1;94mup to date\033[0m with remote repository") #]]
-        if "working tree clean" in status:
-            print("local working tree \033[1;94mclean\033[0m") #]]
+@contextlib.contextmanager
+def new_cd (newdir):
+    d = os.getcwd()
+    os.chdir(newdir)
+    try:
+        yield
+    finally:
+        os.chdir(d)
+
+if (subprocess.call(['git', '-C', args.target, 'status'], stderr = subprocess.STDOUT, stdout = open(os.devnull, 'w')) == 0):
+    with new_cd(args.target):
+        branch = run_command(["git", "rev-parse", "--abbrev-ref", "HEAD"])
+        os.system("git remote update")
+        status = run_command(["git", "status"])
+        print("\033[34m> Checking git repository on branch \033[33m" + branch + "\033[34m...\033[0m") #]]]]
+        if "branch is up to date" in status:
+            print("branch is \033[1;94mup to date\033[0m with remote repository") #]]
+            if "working tree clean" in status:
+                print("local working tree \033[1;94mclean\033[0m") #]]
+            else:
+                print("local changes:")
+                os.system("git status -s")
+                print("\033[34m> Pushing local changes...\033[0m") #]]
+                os.system("git add . && git commit -m \"" + args.message +"\" && git push")
+        elif "not staged" in status:
+            print("local branch falls \033[1;91mbehind\033[0m remote") #]]
+            print("changes \033[1;91mnot staged\033[0m:") #]]
+            os.system("git status -s")
+            print("\033[error:\033[0m Clash between local and remote changes.") #]]
         else:
-            print("local changes:")
-            os.system("git -C " + args.target + " status -s")
-            print("\033[34m> Pushing local changes...\033[0m") #]]
-            os.system("git -C " + args.target + " add . && git -C " + args.target + " commit -m \"" + args.message +"\" && git -C " + args.target + " push")
-    elif "not staged" in status:
-        print("local branch falls \033[1;91mbehind\033[0m remote") #]]
-        print("changes \033[1;91mnot staged\033[0m:") #]]
-        os.system("git -C " + args.target + " status -s")
-        print("\033[error:\033[0m Clash between local and remote changes.") #]]
-    else:
-        print("local branch falls \033[1;94mbehind\033[0m remote") #]]
-        print("\033[34m> Pulling remote changes...\033[0m") #]]
-        os.system("git -C " + args.target + " fetch && git -C " + args.target + " pull")
+            print("local branch falls \033[1;94mbehind\033[0m remote") #]]
+            print("\033[34m> Pulling remote changes...\033[0m") #]]
+            os.system("git fetch && git pull")
 else:
     print("\033[error:\033[0m Not a git repository.") #]]
