b5c527461aa21e330da98465f0e16285d067760b
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-17 10:08:04 +0100

    system rebuild

--- home-ramak/main.nix
+++ home-ramak/main.nix
@@ -33,11 +33,14 @@
                 "application/pdf" = ["org.pwmt.zathura-pdf-mupdf.desktop"];
                 "audio/mpeg" = ["mpv.desktop"];
                 "audio/mp3" = ["mpv.desktop"];
+                "audio/mp3" = ["mpv.desktop"];
+                "video/vnd.avi" = ["mpv.desktop"];
             };
             defaultApplications = {
                 "application/pdf" = ["org.pwmt.zathura-pdf-mupdf.desktop"];
                 "audio/mpeg" = ["mpv.desktop"];
                 "audio/mp3" = ["mpv.desktop"];
+                "video/vnd.avi" = ["mpv.desktop"];
             };
         };
 
