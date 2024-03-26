835bc600b490062fcb79a02d418784ad2b73b4f9
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-03-26 16:03:12 +0100

    system rebuild

--- scripts/parsecmd.py
+++ scripts/parsecmd.py
@@ -12,5 +12,5 @@ parser.add_argument("command", type = str, help = "command to execute")
 parser.add_argument("argument", type = str, help = "argument to execute command with")
 args = parser.parse_args()
 
-cmd = args.command.replace("%", args.argument) if ("%" in args.command) else args.command + " " + args.argument
+cmd = args.command.replace("%", "\""+args.argument+"\"") if ("%" in args.command) else args.command + " \"" + args.argument + "\""
 os.system(cmd)
