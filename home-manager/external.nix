85f470af2246fc430444f50aadc3c4a8055eca6b
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-06-17 21:32:51 +0200

    pshash file force

--- home-manager/external.nix
+++ home-manager/external.nix
@@ -595,7 +595,7 @@
 
         home.file.".local/bin/pshash" =
         let path = /home/ramak/projects/pshash/bin/pshash.nixos.x86_64;
-        in if (builtins.pathExists path) then { source = path; } else { text = "echo 'sorry, no pshash'"; };
+        in if (builtins.pathExists path) then { source = path; force = true; } else { text = "echo 'sorry, no pshash'"; };
 
         # keynav setup
         services.keynav.enable = true;
