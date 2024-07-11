b95688622ae7c1ea6d8c56419c8e6e830e050dc8
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-07-11 17:06:36 +0200

    minor

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -78,26 +78,22 @@ require('lazy').setup({
                 faster = false,
 				map = '<M-/>',
             },
-            internal_pairs = {
-                 { '[',']', fly = true, dosuround = true, newline = true, space = true },
-                 { '(',')', fly = true, dosuround = true, newline = true, space = true },
-                 { '{','}', fly = true, dosuround = true, newline = true, space = true },
-                 { '"', '"', suround = true, multiline = false },
-                 {
-                     "'", "'",
-                     suround = true,
-                     cond = function(fn) return not fn.in_lisp() or fn.in_string() end,
-                     alpha = true,
-                     nft = { 'tex', 'typst', 'haskell' },
-                     multiline = false
-                 },
-                 -- { '$', '$', fly = true, dosuround = true, newline = true, space = true, ft = { 'typst', 'tex' } },
-                 { '```', '```', newline = true, ft = { 'markdown' } },
-                 { '"""', '"""', newline = true, ft = {'python'} },
-                 { "'''", "'''", newline = true, ft = {'python'} },
-            },
-            -- config_internal_pairs = {
-            --     {'\'', '\'', nft = { 'typst', 'haskell' }},
+            -- internal_pairs = {
+            --     { '[',']', fly = true, dosuround = true, newline = true, space = true },
+            --     { '(',')', fly = true, dosuround = true, newline = true, space = true },
+            --     { '{','}', fly = true, dosuround = true, newline = true, space = true },
+            --     { '"', '"', suround = true, multiline = false },
+            --     {
+            --         "'", "'",
+            --         suround = true,
+            --         cond = function(fn) return not fn.in_lisp() or fn.in_string() end,
+            --         alpha = true,
+            --         nft = { 'tex', 'typst' },
+            --         multiline = false
+            --     },
+            --     { '```', '```', newline = true, ft = { 'markdown' } },
+            --     { '"""', '"""', newline = true, ft = {'python'} },
+            --     { "'''", "'''", newline = true, ft = {'python'} },
             -- },
         },
     },
