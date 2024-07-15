6754bee553840e624fca4f6700c68dc26b98d890
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-07-15 17:12:55 +0200

    blkcmd minor

--- scripts/blkcmd.py
+++ scripts/blkcmd.py
@@ -8,7 +8,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--recursive", action = "store_true", help = "search for files recursively in current directory")
 parser.add_argument("-b", "--background", action = "store_true", help = "run commands in background")
 parser.add_argument("-p", "--parse", action = "store_true", help = "use parsecmd to parse commands")
-parser.add_argument("-s", "--silent", action = "store_true", help = "suppress output from commands")
+parser.add_argument("-s", "--silent", action = "store_true", help = "report the commands and files they are ran on")
+parser.add_argument("-n", "--nooutput", action = "store_true", help = "suppress output from commands")
 parser.add_argument("filetype", type = str)
 parser.add_argument("command", type = str)
 args = parser.parse_args()
@@ -25,21 +26,30 @@ for root, dirnames, filenames in os.walk('.'):
     os.chdir(root)
     if (not args.recursive and root != "."):
         continue
-    for filename in fnmatch.filter(filenames, "*."+args.filetype):
-        head_tail = os.path.split(filename)
-        file = root + "/" + filename
-        if (".lib."+args.filetype in head_tail[1]):
-            continue
-        print(CBLUE + "> Executing \"" + CGREEN + args.command + CBLUE + "\" on " + CPURPLE + file + CBLUE + "..." + CEND)
-        if args.parse:
-            os.system(
-                "parsecmd \"" + args.command + (args.background and '&' or '') + "\" \"" + head_tail[1] + "\"" +
-                (args.silent and " > /dev/null 2>&1" or "")
-            )
-        else:
-            os.system(
-                args.command + " " + head_tail[1] +
-                (args.background and '&' or '') +
-                (args.silent and " > /dev/null 2>&1" or '')
-            )
+    if args.filetype == "dir":
+        for dirname in dirnames:
+            os.chdir(dirname)
+            if not args.silent:
+                print(CBLUE + "> Executing \"" + CGREEN + args.command + CBLUE + "\" in " + CPURPLE + root + "/" + dirname + CBLUE + "..." + CEND)
+            os.system(args.command + (args.nooutput and " > /dev/null 2>&1" or ""))
+            os.chdir("..")
+    else:
+        for filename in fnmatch.filter(filenames, "*."+args.filetype):
+            head_tail = os.path.split(filename)
+            file = root + "/" + filename
+            if (".lib."+args.filetype in head_tail[1]):
+                continue
+            if not args.silent:
+                print(CBLUE + "> Executing \"" + CGREEN + args.command + CBLUE + "\" on " + CPURPLE + file + CBLUE + "..." + CEND)
+            if args.parse:
+                os.system(
+                    "parsecmd \"" + args.command + (args.background and '&' or '') + "\" \"" + head_tail[1] + "\"" +
+                    (args.nooutput and " > /dev/null 2>&1" or "")
+                )
+            else:
+                os.system(
+                    args.command + " " + head_tail[1] +
+                    (args.background and '&' or '') +
+                    (args.nooutput and " > /dev/null 2>&1" or "")
+                )
     os.chdir(cwd)
