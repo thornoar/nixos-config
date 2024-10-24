626ab5f8243a9e8134707e4c262c2a0ad43b6883
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-10-24 10:21:55 +0800

    returned typst.vim

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -55,11 +55,11 @@ require('lazy').setup({
             { '<M-g>', ':LazyGit<CR>', desc = 'LazyGit' }
         },
     },
-    -- {
-    --     'kaarmu/typst.vim',
-    --     ft = 'typst',
-    --     lazy = false,
-    -- },
+    {
+        'kaarmu/typst.vim',
+        ft = 'typst',
+        lazy = false,
+    },
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.6',
         dependencies = { 'nvim-lua/plenary.nvim' }
