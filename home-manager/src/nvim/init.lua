8a2be1ee9b5111ed47f0a7d297e9d14eec9875c4
Roman Maksimovich <r.a.maksimovich@gmail.com>
2025-02-16 09:46:29 +0800

    automatic commit

--- home-manager/src/nvim/init.lua
+++ home-manager/src/nvim/init.lua
@@ -36,8 +36,10 @@ require('lazy').setup({
     'chrisbra/csv.vim',
     'JuliaEditorSupport/julia-vim',
     'mbbill/undotree',
-    'agda/cornelis',
-    'kana/vim-textobj-user',
+    -- 'neovimhaskell/nvim-hs.vim',
+    -- 'agda/cornelis',
+    -- 'kana/vim-textobj-user',
+    'Isti115/agda.nvim',
     {
         'stevearc/oil.nvim',
         ---@module 'oil'
