94cf4fe0514b0d0496ddf1543523db0399cdebbf
Roman Maksimovich <r.a.maksimovich@gmail.com>
2025-08-21 12:12:06 +0200

    21 Aug 2025 (Thu): 12:12

--- nixos/modules/laptop/specialisations.nix
+++ nixos/modules/laptop/specialisations.nix
@@ -1,4 +1,4 @@
-args: {
+{ pkgs, pkgs-unstable, lib, readPackages, config, ... }@args: {
   specialisation.hyprland.configuration = import ./hyprland.nix args;
   specialisation.hyprland-powersave.configuration = import ./hyprland-powersave.nix args;
 }
