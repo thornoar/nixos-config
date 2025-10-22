603bae03484bfdf14f3a3d12768f5cb98a1bd7d0
Roman Maksimovich <r.a.maksimovich@gmail.com>
2025-10-22 22:12:25 +0800

    22 Oct 2025 (Wed): 22:12

--- home-manager/src/scripts/gitall.sh
+++ home-manager/src/scripts/gitall.sh
@@ -3,28 +3,23 @@
 curdir=$(pwd)
 cd "$HOME/projects" || exit
 blkcmd dir "git status -s" -c
-cancelled=0
+dirty=0
 
-function ask {
+for dir in ./*; do
+    cd "$dir" || exit
+    if git diff-index --quiet HEAD; then
+        dirty=1
+    fi
+    cd .. || exit
+done
+
+if [ $dirty = 1 ]; then
     read -r -p "? Commit & push all changes? (y/N) " answer
     answer=${answer:-Y}
     if [[ "$answer" =~ ^[Yy]$ ]]; then
         cd "$HOME/projects" || exit
         blkcmd dir "gitcommit" -c
-    else
-        cancelled=1
     fi
-}
-
-for dir in ./*; do
-    (
-        cd "$dir" || exit
-        git diff-index --quiet HEAD || ask
-        if [ $cancelled = 1 ]; then
-            echo "> Cancelled."
-            exit 0
-        fi
-    )
-done
+fi
 
 cd "$curdir" || exit
