4215ce705f40f6290c50433e06966e81da3d8fbc
Roman Maksimovich <r.a.maksimovich@gmail.com>
2025-12-29 22:32:32 +0100

    29 Dec 2025 (Mon): 22:32

--- home-manager/modules/niri.nix
+++ home-manager/modules/niri.nix
@@ -1,4 +1,18 @@
 { config, lib, ... }:
-{
+let
+  clr = config.colors;
+  ts = builtins.toString;
+in {
   xdg.configFile."niri/config.kdl" = config.util.dotFileMut "niri/config.kdl";
+  xdg.configFile."niri/share.kdl".text = ''
+    layout {
+      gaps ${ts config.window.windowSpaceOuter}
+      border {
+        width 1
+        active-color "${clr.primary}"
+        inactive-color "${clr.bg0}"
+        urgent-color "${clr.red1}"
+      }
+    }
+  '';
 }
--- home-manager/src/scripts/niri-scratch.sh
+++ home-manager/src/scripts/niri-scratch.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-scratch_wsp="scratch"
-cur_wsp="$(niri msg -j workspaces | jq '.[] | select(.is_active == true)' | jq '.name')"
+# scratch_wsp_id="$(niri msg -j workspaces | jq ".[] | select(.name == \"$1\").id")"
+scratch_win_id="$(niri msg -j windows | jq ".[] | select(.title == \"$1\").id")"
 
-if [ "$cur_wsp" != "$scratch_wsp" ]; then
-    niri msg action focus-workspace "$scratch_wsp"
+if [ -z "$scratch_win_id" ]; then
+    echo "$scratch_win_id"
+    exit 0
+fi
+
+# cur_wsp_id="$(niri msg -j workspaces | jq '.[] | select(.is_active == true).id')"
+cur_win_id="$(niri msg -j windows | jq '.[] | select(.is_focused == true).id')"
+
+if [[ "$cur_win_id" != "$scratch_win_id" ]]; then
+    # if [[ "$cur_wsp_id" != "$scratch_wsp_id" ]]; then
+    #     niri msg action focus-workspace --id "$1"
+    # fi
+    niri msg action focus-window --id "$scratch_win_id"
 else
     niri msg action focus-workspace-previous
 fi
