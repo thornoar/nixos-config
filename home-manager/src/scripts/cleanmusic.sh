d002019d04b84f6ff0beeb0678361c4fa27e2b2f
Roman Maksimovich <r.a.maksimovich@gmail.com>
2026-02-27 10:42:06 +0800

    27 Feb 2026 (Fri): 10:42

--- home-manager/src/scripts/cleanmusic.sh
+++ home-manager/src/scripts/cleanmusic.sh
@@ -50,102 +50,6 @@ function patch_flac {
     done
 }
 
-function orphan_m4a {
-    for file in ./**/*.m4a; do
-        if test ! -f "$file"; then
-            continue
-        fi
-        pref="${file%*.m4a}"
-        if test ! -f "$pref.lrc" -a ! -f "$pref.txt"; then
-            echo "$file"
-        fi
-    done
-}
-
-function search_m4a {
-    im4a=1
-    for file in ./**/*.m4a; do
-        if test ! -f "$file"; then
-            continue
-        fi
-        pref="${file%*.m4a}"
-        artist=""
-        title=""
-        if test ! -f "$pref.lrc" -a ! -f "$pref.txt"; then
-            artist="$(ffprobe -v quiet -show_entries format_tags=artist -of default=noprint_wrappers=1:nokey=1 "$file")"
-            title="$(ffprobe -v quiet -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 "$file")"
-            echo "$iflac $artist - $title"
-            syncedlyrics "$artist - $title" --synced-only -o "$pref.lrc" > /dev/null
-        fi
-        if test ! -f "$pref.lrc" -a ! -f "$pref.txt"; then
-            echo "-- trying plain lyrics"
-            syncedlyrics "$artist - $title" --plain-only -o "$pref.txt" > /dev/null
-        fi
-        ((im4a++))
-    done
-}
-
-function patch_m4a {
-    for file in ./**/*.m4a; do
-        if test ! -f "$file"; then
-            continue
-        fi
-        pref="${file%*.m4a}"
-        if test ! -f "$pref.lrc" -a ! -f "$pref.txt"; then
-            echo "$file"
-            echo "[au: instrumental]" > "$pref.lrc"
-        fi
-    done
-}
-
-function orphan_mp3 {
-    for file in ./**/*.mp3; do
-        if test ! -f "$file"; then
-            continue
-        fi
-        pref="${file%*.mp3}"
-        if test ! -f "$pref.lrc" -a ! -f "$pref.txt"; then
-            echo "$file"
-        fi
-    done
-}
-
-function search_mp3 {
-    imp3=1
-    for file in ./**/*.mp3; do
-        if test ! -f "$file"; then
-            continue
-        fi
-        pref="${file%*.mp3}"
-        artist=""
-        title=""
-        if test ! -f "$pref.lrc" -a ! -f "$pref.txt"; then
-            artist="$(ffprobe -v quiet -show_entries format_tags=artist -of default=noprint_wrappers=1:nokey=1 "$file")"
-            title="$(ffprobe -v quiet -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 "$file")"
-            echo "$iflac $artist - $title"
-            syncedlyrics "$artist - $title" --synced-only -o "$pref.lrc" > /dev/null
-        fi
-        if test ! -f "$pref.lrc" -a ! -f "$pref.txt"; then
-            echo "-- trying plain lyrics"
-            syncedlyrics "$artist - $title" --plain-only -o "$pref.txt" > /dev/null
-        fi
-        ((imp3++))
-    done
-}
-
-function patch_mp3 {
-    for file in ./**/*.mp3; do
-        if test ! -f "$file"; then
-            continue
-        fi
-        pref="${file%*.mp3}"
-        if test ! -f "$pref.lrc" -a ! -f "$pref.txt"; then
-            echo "$file"
-            echo "[au: instrumental]" > "$pref.lrc"
-        fi
-    done
-}
-
 orphan=0
 search=0
 patch=0
@@ -160,18 +64,12 @@ done
 
 if [ $orphan == 1 ]; then
     orphan_flac
-    orphan_m4a
-    orphan_mp3
 fi
 
 if [ $search == 1 ]; then
     search_flac
-    search_m4a
-    search_mp3
 fi
 
 if [ $patch == 1 ]; then
     patch_flac
-    patch_m4a
-    patch_mp3
 fi
