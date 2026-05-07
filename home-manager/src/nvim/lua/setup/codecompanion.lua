912715ef7dda64ce768d61ae77a64f66ee632e81
Roman Maksimovich <r.a.maksimovich@gmail.com>
2026-05-07 16:24:26 +0800

    07 May 2026 (Thu): 16:24

--- home-manager/src/nvim/lua/setup/codecompanion.lua
+++ home-manager/src/nvim/lua/setup/codecompanion.lua
@@ -1,37 +1,46 @@
-require("codecompanion").setup({
-    strategies = {
-        chat = {
-            adapter = "copilot",
-            keymaps = {
-                send = {
-                    modes = { n = "<C-s>", i = "<C-p>" },
+return {
+    "olimorris/codecompanion.nvim",
+    -- dependencies = {
+    --     "ravitemer/mcphub.nvim"
+    -- },
+    config = function ()
+        require("codecompanion").setup({
+            strategies = {
+                chat = {
+                    adapter = "copilot",
+                    keymaps = {
+                        send = {
+                            modes = { n = "<C-s>", i = "<C-p>" },
+                        },
+                        close = {
+                            modes = { n = "<C-c>", i = "<C-p>" },
+                        },
+                    },
                 },
-                close = {
-                    modes = { n = "<C-c>", i = "<C-p>" },
+                inline = {
+                    adapter = "copilot",
                 },
             },
-        },
-        inline = {
-            adapter = "copilot",
-        },
-    },
-    display = {
-        action_palette = {
-            width = 95,
-            height = 10,
-            prompt = "Prompt: ", -- Prompt used for interactive LLM calls
-            provider = "default", -- default|telescope|mini_pick
-            opts = {
-                show_default_actions = true, -- Show the default actions in the action palette?
-                show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+            display = {
+                action_palette = {
+                    width = 95,
+                    height = 10,
+                    prompt = "Prompt: ", -- Prompt used for interactive LLM calls
+                    provider = "default", -- default|telescope|mini_pick
+                    opts = {
+                        show_default_actions = true, -- Show the default actions in the action palette?
+                        show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+                    },
+                },
+                chat = {
+                    window = {
+                        layout = "horizontal",
+                    },
+                },
             },
-        },
-        chat = {
-            window = {
-                layout = "vertical",
-            },
-        },
-    },
-})
-vim.keymap.set("n", "<C-S-a>", "<cmd>CodeCompanionActions<CR>")
-vim.keymap.set("n", "<C-S-s>", "<cmd>CodeCompanionChat<CR>")
+        })
+        vim.keymap.set("n", "<C-S-a>", "<cmd>CodeCompanionActions<CR>")
+        vim.keymap.set("n", "<C-S-s>", "<cmd>CodeCompanionChat<CR>")
+    end
+}
+
