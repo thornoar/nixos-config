70e27bad9a5ae9a3696f4169382dd4835aeb6642
Roman Maksimovich <r.a.maksimovich@gmail.com>
2026-09-11 16:23:53 +0800

    11 Sep 2026 (Fri): 16:23

--- home-manager/src/scripts/reformat.sh
+++ home-manager/src/scripts/reformat.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+shopt -s globstar
+
 for file in ./**/*."$1"; do
     stripped="${file%.*}"
-    ffmpeg -i "$file" "$stripped.$2"
+    ffmpeg -i "$file" "$stripped.$2" $3
 done
